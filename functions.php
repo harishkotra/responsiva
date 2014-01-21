2b17cc2f30812efb1a9dc996857a3d1c7f13b61a
Harish Kotra <harishkotra@gmail.com>
2014-01-21 16:47:09 +0530

    Security fixes for wordpress and also enabled threaded commenting on each post.

--- functions.php
+++ functions.php
@@ -1,5 +1,25 @@
 <?php
 
+// remove junk from head
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'feed_links', 2);
+remove_action('wp_head', 'index_rel_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'parent_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link', 10, 0);
+
+// enable threaded comments
+function enable_threaded_comments(){
+	if (!is_admin()) {
+		if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1))
+			wp_enqueue_script('comment-reply');
+		}
+}
+add_action('get_header', 'enable_threaded_comments');
+
 //function to remove the version from displaying in wordpress.
 function responsiva_remove_version() {
 return '';
@@ -7,5 +27,15 @@ return '';
 add_filter('the_generator', 'responsiva_remove_version');
 
 
+//Two menu locations definition
 
+function register_responsiva_menus() {
+  register_nav_menus(
+    array(
+      'header-menu' => __( 'Header Menu' ),
+      'footer-menu' => __( 'Footer Menu' )
+    )
+  );
+}
+add_action( 'init', 'register_responsiva_menus' );
 ?>
