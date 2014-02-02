4acc8d4f780a6f8be20ef2227bc60fb61b6a2bc6
Harish Kotra <harish@deepredink.com>
2014-02-02 17:22:15 +0530

    Added custom sidebar with default widgets.

--- sidebar.php
+++ sidebar.php
@@ -1,33 +1,39 @@
 <aside id="sidebar-wrapper">
+
   <nav class="sidebar">
-   <h1>We are Social !</h1>
-   <ul>
-    <li><a href="http://www.facebook.com/dzyngiri" target="_blank">Facebook</a>
-    <li><a href="http://twitter.com/dzyngiri" target="_blank">Twitter</a>
-   </ul>
+
+    <?php if ( is_active_sidebar( 'main-sidebar' ) ) : ?>
+
+      <?php dynamic_sidebar( 'main-sidebar' ); ?>
+
+    <?php else : ?>
+
+        <nav class="sidebar">
+         <h1>I am Social !</h1>
+         <ul>
+          <li><a href="http://www.facebook.com/harish.kotra" target="_blank">Facebook</a>
+          <li><a href="http://twitter.com/HarishKotra" target="_blank">Twitter</a>
+         </ul>
+        </nav>
+        
+        <nav class="sidebar">
+          
+          <h1>Calendar</h1>
+          <ul>
+          <li id="calendar"><?php get_calendar(); ?></li>
+          </ul>
+
+          <?php wp_list_pages('title_li=<h1>Pages</h1>'); ?>
+          
+          <h1><?php _e('Categories'); ?></h1>
+          <ul>
+            <?php wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
+          </ul> 
+        </nav>
+
+    <?php endif; ?>
+
   </nav>
-  <nav class="sidebar">
-   <h1>Recent Posts</h1>
-   <ul>
-    <li><a href="http://www.dzyngiri.com/?p=774" target="_blank">How to make website Readable [Tips]</a></li>
-    <li><a href="http://www.dzyngiri.com/?p=760" target="_blank">Gliding Divs Using jParallax Plugin [Tutorial]</a></li>
-    <li><a href="http://www.dzyngiri.com/?p=752" target="_blank">Comparing HTML5 Mobile Web Framework</a></li>
-    <li><a href="http://www.dzyngiri.com/?p=701" target="_blank">Free Logo PSD Templates [Freebies]</a></li>
-    <li><a href="http://www.dzyngiri.com/?p=682" target="_blank">Stunning Modal Web PopUp [Freebies]</a></li>
-   </ul>
-  </nav>
- 
- <!-- Sidebar Element Ends Here -->
- 
- <!-- Another Sidebar Element Starts Here -->
- <section class="sidebar">
-  <h1>Advertisement</h1>
-  <!-- BuySellAds Zone Code -->
-  <div style="margin-top:15px; margin-left:30px;">
-<div id="bsap_1274839" class="bsarocks bsap_7e34fb77f06a911111decc6ffaf24d16"></div>
-</div>
-<!-- End BuySellAds Zone Code -->
-  
-  </section>
+
  </aside>
 <!-- Another Sidebar Element Ends Here -->
